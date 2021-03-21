095be46af3097ea2f32692c9326b3486d2d6e567
Gipria <giovana_alves100@live.com>
2021-03-21 19:57:31 -0300

    noticias,js

--- noticias.js
+++ noticias.js
@@ -1,7 +1,20 @@
 var http = require('http');
 
 var server = http.createServer(function(req,res){
+    var categoria = req.url;
+    
+    if(categoria == '/tecnologia'){
+        res.end("<html><body>Noticias de Tecnologia")
+    
+    } else if (categoria == '/moda'){
+        res.end("<html><body>Noticias de Moda")
+
+    } else if (categoria == '/beleza'){
+        res.end("<html><body>Noticias de beleza</body></html>")
+
+    }else {
     res.end("<html><body>Portal de noticias</body></html>");
+    }
 
 });
 
